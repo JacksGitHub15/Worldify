751f361e0400eecb377ecf3b2f5c58a709ea928e
Ben Reilly <benreilly6544@gmail.com>
2020-11-23 21:51:46 +0000

    music search with artist/album/playlist filter

--- client/src/components/RedirectPage.js
+++ client/src/components/RedirectPage.js
@@ -2,16 +2,12 @@ import React from 'react';
 import _ from 'lodash';
 import { getParamValues } from '../utils/functions';
 
-
-// componentDidMount lifecycle is a method to access the URL parameters and store them in local storage
-// We're calling the getParamValues function by passing the URL values available in location.hash
-// expires_in value is in seconds so we convert it to milliseconds by multiplying it by 1000 and then adding it to the milliseconds at the current time,
-// meaning it has an expiryTime of 1 hour
 export default class RedirectPage extends React.Component {
   componentDidMount() {
     const { setExpiryTime, history, location } = this.props;
     try {
       if (_.isEmpty(location.hash)) {
+        setExpiryTime(expiryTime);
         return history.push('/dashboard');
       }
       const access_token = getParamValues(location.hash);
@@ -27,3 +23,5 @@ export default class RedirectPage extends React.Component {
     return null;
   }
 }
+
+
