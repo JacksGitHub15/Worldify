8db35ebf039faeddd1b2363df4db758068752cd7
Craig Stratford <craigastratford@gmail.com>
2020-11-27 17:23:45 +0000

    :hammer: Allows debugging from ReduxDevTools extension

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,17 +1,17 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import albumsReducer from '../reducers/albums';
 import artistsReducer from '../reducers/artists';
 import playlistReducer from '../reducers/playlist';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const store = createStore(
   combineReducers({
     albums: albumsReducer,
     artists: artistsReducer,
     playlist: playlistReducer
-  }),
+  }, devToolsEnhancer),
   composeEnhancers(applyMiddleware(thunk))
 );
 
